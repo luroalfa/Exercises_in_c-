83d811412b16c6872a7ddf72bc142ac6599ec7cd
luroalfa <rodriguezfallasluis@gmail.com>
2022-11-16 10:29:47 -0600

    Third exercises is done

---
+++ ejercicio3.cpp
@@ -0,0 +1,28 @@
+/*
+    Author:
+        Luis A. Rodríguez F.
+    Date:
+        Wednesday, November 16th, 2022.
+    Last edit:
+        Wednesday, November 16th, 2022.
+    Description:
+        Escriba un programa que solicite la medida del radio de un círculo y calcule el área y el perímetro,
+        los resultados se deben mostrar con dos decimales (investigue como mostrar números con n cantidad de decimales).
+*/
+// Libraries
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// Main function
+int main()
+{
+    double radius, area, perimeter;
+    cout << "Ingrese el radio del circulo.\n";
+    cin >> radius;
+    // Mathematical calculation for area is: π * r² and perimeter is: 2*π*r
+    area = M_PI * pow(radius, 2);
+    perimeter = 2 * M_PI * radius;
+    printf("%s%.2f%s", "El valor del área del circulo es: ", area, "cm²\n");
+    printf("%s%.2f%s", "El valor del perimetro del circulo es: ", perimeter, "cm\n");
+}
